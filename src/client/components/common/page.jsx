5f8f9d950ecb853c9fb8f2a5ee13daf493089e76
Alex Kalina <kalinalex91@gmail.com>
2017-12-11 10:45:37 +0300

    [Scaffolding] Ping Fang font was added

--- src/client/components/common/page.jsx
+++ src/client/components/common/page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css"
+import "../../styles/fonts.css"
+import "../../styles/main.css"
+
 
 class Page extends React.Component {
   render() {
