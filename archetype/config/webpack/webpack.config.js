a383fb8fb0e1a0fc536ef04f5eeb2dbaa17ccf71
Alex Kalina <kalinalex91@gmail.com>
2018-01-12 05:04:19 +0300

    [Build] The support of ES6 dependency sources uglification was added.

--- archetype/config/webpack/webpack.config.js
+++ archetype/config/webpack/webpack.config.js
@@ -1,15 +1,24 @@
 const extractStyle = require("./partials/extract-style");
-//TODO Find out more elegant way to add global css
+const uglify = require("./partials/uglify");
+
 module.exports = function (composer, options, compose) {
   composer.addPartials({
     "custom-style": {
       config: extractStyle()
+    },
+    "custom-uglify": {
+      config: uglify()
     }
   });
   const base = composer.profiles._base;
   const tmp = base.partials["_extract-style"];
   tmp.enable = false;
   base.partials["custom-style"] = {order: tmp.order};
+
+  const production = composer.profiles._production;
+  const tmpUglify = production.partials["_uglify"];
+  tmpUglify.enable = false;
+  production.partials["custom-uglify"] = {order: tmpUglify.order};
 
   return compose();
 };
