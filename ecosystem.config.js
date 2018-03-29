deb55f92f9f0ef59d7f0050331dbd6dce7703af2
Alex Kalina <kalinalex91@gmail.com>
2018-03-29 14:34:28 +0300

    [Deploy] Fix deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,7 @@ module.exports = {
     stage: {
       user: "deployer",
       host: "92.53.66.193",
+      "ssh_options": "ForwardAgent=yes",
       ref: "origin/develop",
       repo: "git@github.com:EvercodeLab/qtum-web.git",
       path: "/var/www/qtum",
@@ -27,6 +28,7 @@ module.exports = {
       user: "deployer",
       host: "192.168.53.210",
       ref: "origin/develop",
+      "ssh_options": "ForwardAgent=yes",
       repo: "git@github.com:inklabsfoundation/ink-wallet-web.git",
       path: "/var/www/wallet.ink.one",
       "post-deploy": "yarn install --production=false && ENABLE_NODESOURCE_PLUGIN=true clap build && pm2 startOrRestart ecosystem.config.js --env production && pm2 save",
