532d9b3156bfa4223c8b9a2aed85b805089307ee
margarita-f <ashara.unerkannt@gmail.com>
2018-04-10 18:46:41 +0300

    Pending Label in Chinese is changed

--- src/client/locale/dictionary.js
+++ src/client/locale/dictionary.js
@@ -426,7 +426,7 @@ export const dictionary = {
     },
     assetsDetails: {
       amountLabel: "可用余额：",
-      pendingLabel: "等待中：",
+      pendingLabel: "发送等待中：",
       allTabLabel: "全部",
       sendTabLabel: "已发送",
       receivedTabLabel: "已接收",
